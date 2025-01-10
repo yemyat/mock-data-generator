32c82bb6ef69bca27796cc40e5c7dc888d657f9d
Ye Myat Min <yemyat@gmail.com>
2025-01-10 15:27:50 +0800

    chore: remove scan

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
-      </head>
       <body
         className={`${instrumentSans.variable} ${firaCode.variable} antialiased`}
       >
